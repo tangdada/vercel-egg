53170ecff57ca135e2a65191a01eb25a44b66918
tangdada <361134446@qq.com>
2022-04-12 19:21:56 +0800

    Update _workingHour.js

--- api/db/model/_workingHour.js
+++ api/db/model/_workingHour.js
@@ -1,32 +1,40 @@
 const WorkHourSchema = {
     id: {
         type: String,
-        required: true,
+        required: true
     },
     startTime: {
         type: String,
-        required: true,
+        required: true
     },
     endTime: {
         type: String,
-        required: true,
+        required: true
     },
     job: {
+        type: String
+    },
+    company: {
         type: String,
+        maxlength: [16, '公司名称不能超过16个字']
     },
     gender: {
         type: String,
-        maxlength: ['男', '女'],
+        maxlength: ['男', '女']
     },
     age: {
+        type: String
+    },
+    remark: {
         type: String,
+        maxlength: [140, '备注不能超过140个字']
     },
     createTime: {
-        type: Number,
+        type: Number
     },
     updateTime: {
-        type: Number,
-    },
+        type: Number
+    }
 }
 
 module.exports = WorkHourSchema
