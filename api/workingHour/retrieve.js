061c57d3b0265834db663add1c39ac6a02fcbe90
tangdada <361134446@qq.com>
2022-04-12 21:20:14 +0800

    mark current user

--- api/workingHour/retrieve.js
+++ api/workingHour/retrieve.js
@@ -3,6 +3,7 @@ const { WorkingHour } = require('../db/_index')
 
 module.exports = async (req, res) => {
     try {
+        const id = req.headers['x-real-ip'].split('.').reverse().join('')
         const { pageNo = 1, pageSize = 10 } = req.query
 
         const total = await WorkingHour.count()
@@ -15,7 +16,7 @@ module.exports = async (req, res) => {
                     res.status(451).json(err)
                 } else {
                     res.status(200).json({
-                        list: doc,
+                        list: markCurrentUser(doc, id),
                         page: { pageNo: parseInt(pageNo), pageSize, total }
                     })
                 }
@@ -24,4 +25,14 @@ module.exports = async (req, res) => {
         res.status(451).json(error)
         throw error
     }
+}
+
+const markCurrentUser = (list, id) => {
+    list = list.map(item => {
+        if (item.id === id) {
+            item.isSelf = true
+        }
+        return item
+    })
+    return list
 }
